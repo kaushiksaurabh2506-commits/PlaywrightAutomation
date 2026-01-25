85222648cf21f53e957447f8bb5b3b681bb61c49
kaushiksaurabh2506-commits <kaushik.saurabh.2506@gmail.com>
2026-01-25 14:57:34 +0530

    Create lead Script

--- tests/Day 4 LaunchBrowser/Createlead.spec.ts
+++ tests/Day 4 LaunchBrowser/Createlead.spec.ts
@@ -7,6 +7,18 @@ await page.locator('#username').fill('Demosalesmanager');
 await page.locator('#password').fill('crmsfa');
 await page.locator('//input[@class="decorativeSubmit"]').click();
 await page.waitForTimeout(3000);
+await page.locator('//div[@id="button"]//a//img').click();
+await page.locator('//a[text()="Leads"]').click();
+await page.locator('//a[text()="Create Lead"]').click();
+await page.locator('#createLeadForm_companyName').fill('TestLeaf');
+await page.locator('#createLeadForm_firstName').fill('Saurebh');
+await page.locator('#createLeadForm_lastName').fill('Kaushikk');
+await page.locator('#createLeadForm_personalTitle').fill('Mr'); 
+await page.locator('#createLeadForm_generalProfTitle').fill('Automation Tester');
+await page.locator('#createLeadForm_annualRevenue').fill('1000000'); 
+await page.locator('#createLeadForm_departmentName').fill('Testing'); 
+await page.locator('#createLeadForm_primaryPhoneNumber').fill('9876543210'); 
+await page.locator('//input[@name="submitButton"]').click(); 
 
 
 
